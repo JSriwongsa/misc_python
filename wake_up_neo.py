5cb9518dc502ffbcb445f46b705c93fd03384452
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-05-19 14:12:26 -0500

    Added a program using the time library to print a message for neo to wake up

---
+++ wake_up_neo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import time
+print('...')
+time.sleep(3)
+print('.....')
+time.sleep(3)
+print('Wake up Neo!')
+time.sleep(3)
+print('Follow the white rabbit.')
+time.sleep(3)
+print('.......')
